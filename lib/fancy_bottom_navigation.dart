c9d04cae52ea1b8cd652feb008591dcb87b66178
Mark Johnson <mark@fireseed.com>
2021-05-05 13:47:06 +0100

    Allow for SafeAreas

--- lib/fancy_bottom_navigation.dart
+++ lib/fancy_bottom_navigation.dart
@@ -23,9 +23,7 @@ class FancyBottomNavigation extends StatefulWidget {
       this.inactiveIconColor,
       this.textColor,
       this.barBackgroundColor})
-      : assert(onTabChangedListener != null),
-        assert(tabs != null),
-        assert(tabs.length > 1 && tabs.length < 5);
+      : assert(tabs.length > 1 && tabs.length < 5);
 
   final Function(int position) onTabChangedListener;
   final Color? circleColor;
@@ -42,8 +40,7 @@ class FancyBottomNavigation extends StatefulWidget {
   FancyBottomNavigationState createState() => FancyBottomNavigationState();
 }
 
-class FancyBottomNavigationState extends State<FancyBottomNavigation>
-    with TickerProviderStateMixin, RouteAware {
+class FancyBottomNavigationState extends State<FancyBottomNavigation> with TickerProviderStateMixin, RouteAware {
   IconData nextIcon = Icons.search;
   IconData activeIcon = Icons.search;
 
@@ -64,27 +61,16 @@ class FancyBottomNavigationState extends State<FancyBottomNavigation>
     activeIcon = widget.tabs[currentSelected].iconData;
 
     circleColor = widget.circleColor ??
-        ((Theme.of(context).brightness == Brightness.dark)
-            ? Colors.white
-            : Theme.of(context).primaryColor);
+        ((Theme.of(context).brightness == Brightness.dark) ? Colors.white : Theme.of(context).primaryColor);
 
-    activeIconColor = widget.activeIconColor ??
-        ((Theme.of(context).brightness == Brightness.dark)
-            ? Colors.black54
-            : Colors.white);
+    activeIconColor =
+        widget.activeIconColor ?? ((Theme.of(context).brightness == Brightness.dark) ? Colors.black54 : Colors.white);
 
     barBackgroundColor = widget.barBackgroundColor ??
-        ((Theme.of(context).brightness == Brightness.dark)
-            ? Color(0xFF212121)
-            : Colors.white);
-    textColor = widget.textColor ??
-        ((Theme.of(context).brightness == Brightness.dark)
-            ? Colors.white
-            : Colors.black54);
+        ((Theme.of(context).brightness == Brightness.dark) ? Color(0xFF212121) : Colors.white);
+    textColor = widget.textColor ?? ((Theme.of(context).brightness == Brightness.dark) ? Colors.white : Colors.black54);
     inactiveIconColor = (widget.inactiveIconColor) ??
-        ((Theme.of(context).brightness == Brightness.dark)
-            ? Colors.white
-            : Theme.of(context).primaryColor);
+        ((Theme.of(context).brightness == Brightness.dark) ? Colors.white : Theme.of(context).primaryColor);
   }
 
   @override
@@ -107,112 +93,117 @@ class FancyBottomNavigationState extends State<FancyBottomNavigation>
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      overflow: Overflow.visible,
-      alignment: Alignment.bottomCenter,
-      children: <Widget>[
-        Container(
-          height: BAR_HEIGHT,
-          decoration: BoxDecoration(color: barBackgroundColor, boxShadow: [
-            BoxShadow(
-                color: Colors.black12, offset: Offset(0, -1), blurRadius: 8)
-          ]),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: widget.tabs
-                .map((t) => TabItem(
-                    uniqueKey: t.key,
-                    selected: t.key == widget.tabs[currentSelected].key,
-                    iconData: t.iconData,
-                    title: t.title,
-                    iconColor: inactiveIconColor,
-                    textColor: textColor,
-                    callbackFunction: (uniqueKey) {
-                      int selected = widget.tabs
-                          .indexWhere((tabData) => tabData.key == uniqueKey);
-                      widget.onTabChangedListener(selected);
-                      _setSelected(uniqueKey);
-                      _initAnimationAndStart(_circleAlignX, 1);
-                    }))
-                .toList(),
-          ),
-        ),
-        Positioned.fill(
-          top: -(CIRCLE_SIZE + CIRCLE_OUTLINE + SHADOW_ALLOWANCE) / 2,
-          child: Container(
-            child: AnimatedAlign(
-              duration: Duration(milliseconds: ANIM_DURATION),
-              curve: Curves.easeOut,
-              alignment: Alignment(_circleAlignX, 1),
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 15),
-                child: FractionallySizedBox(
-                  widthFactor: 1 / widget.tabs.length,
-                  child: GestureDetector(
-                    onTap: widget.tabs[currentSelected].onclick as void
-                        Function()?,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: <Widget>[
-                        SizedBox(
-                          height:
-                              CIRCLE_SIZE + CIRCLE_OUTLINE + SHADOW_ALLOWANCE,
-                          width:
-                              CIRCLE_SIZE + CIRCLE_OUTLINE + SHADOW_ALLOWANCE,
-                          child: ClipRect(
-                              clipper: HalfClipper(),
-                              child: Container(
-                                child: Center(
-                                  child: Container(
-                                      width: CIRCLE_SIZE + CIRCLE_OUTLINE,
-                                      height: CIRCLE_SIZE + CIRCLE_OUTLINE,
-                                      decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          shape: BoxShape.circle,
-                                          boxShadow: [
-                                            BoxShadow(
-                                                color: Colors.black12,
-                                                blurRadius: 8)
-                                          ])),
-                                ),
-                              )),
-                        ),
-                        SizedBox(
-                            height: ARC_HEIGHT,
-                            width: ARC_WIDTH,
-                            child: CustomPaint(
-                              painter: HalfPainter(barBackgroundColor),
-                            )),
-                        SizedBox(
-                          height: CIRCLE_SIZE,
-                          width: CIRCLE_SIZE,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle, color: circleColor),
-                            child: Padding(
-                              padding: const EdgeInsets.all(0.0),
-                              child: AnimatedOpacity(
-                                duration:
-                                    Duration(milliseconds: ANIM_DURATION ~/ 5),
-                                opacity: _circleIconAlpha,
-                                child: Icon(
-                                  activeIcon,
-                                  color: activeIconColor,
-                                ),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+    final safeAreaBottom = MediaQuery.of(context).padding.bottom;
+
+    return Container(
+      decoration: BoxDecoration(
+          color: barBackgroundColor,
+          boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(0, -1), blurRadius: 8)]),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          SafeArea(
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.bottomCenter,
+              children: <Widget>[
+                Container(
+                  height: BAR_HEIGHT,
+                  decoration: null,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: widget.tabs
+                        .map((t) => TabItem(
+                            uniqueKey: t.key,
+                            selected: t.key == widget.tabs[currentSelected].key,
+                            iconData: t.iconData,
+                            title: t.title,
+                            iconColor: inactiveIconColor,
+                            textColor: textColor,
+                            callbackFunction: (uniqueKey) {
+                              int selected = widget.tabs.indexWhere((tabData) => tabData.key == uniqueKey);
+                              widget.onTabChangedListener(selected);
+                              _setSelected(uniqueKey);
+                              _initAnimationAndStart(_circleAlignX, 1);
+                            }))
+                        .toList(),
                   ),
                 ),
-              ),
+                Positioned.fill(
+                  top: -(CIRCLE_SIZE + CIRCLE_OUTLINE + SHADOW_ALLOWANCE) / 2,
+                  child: Container(
+                    child: AnimatedAlign(
+                      duration: Duration(milliseconds: ANIM_DURATION),
+                      curve: Curves.easeOut,
+                      alignment: Alignment(_circleAlignX, 1),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 15),
+                        child: FractionallySizedBox(
+                          widthFactor: 1 / widget.tabs.length,
+                          child: GestureDetector(
+                            onTap: widget.tabs[currentSelected].onclick as void Function()?,
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: CIRCLE_SIZE + CIRCLE_OUTLINE + SHADOW_ALLOWANCE,
+                                  width: CIRCLE_SIZE + CIRCLE_OUTLINE + SHADOW_ALLOWANCE,
+                                  child: ClipRect(
+                                      clipper: HalfClipper(),
+                                      child: Container(
+                                        child: Center(
+                                          child: Container(
+                                              width: CIRCLE_SIZE + CIRCLE_OUTLINE,
+                                              height: CIRCLE_SIZE + CIRCLE_OUTLINE,
+                                              decoration: BoxDecoration(
+                                                  color: Colors.white,
+                                                  shape: BoxShape.circle,
+                                                  boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 8)])),
+                                        ),
+                                      )),
+                                ),
+                                SizedBox(
+                                    height: ARC_HEIGHT,
+                                    width: ARC_WIDTH,
+                                    child: CustomPaint(
+                                      painter: HalfPainter(barBackgroundColor),
+                                    )),
+                                SizedBox(
+                                  height: CIRCLE_SIZE,
+                                  width: CIRCLE_SIZE,
+                                  child: Container(
+                                    decoration: BoxDecoration(shape: BoxShape.circle, color: circleColor),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(0.0),
+                                      child: AnimatedOpacity(
+                                        duration: Duration(milliseconds: ANIM_DURATION ~/ 5),
+                                        opacity: _circleIconAlpha,
+                                        child: Icon(
+                                          activeIcon,
+                                          color: activeIconColor,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
-        )
-      ],
+          Container(
+            height: safeAreaBottom,
+            color: barBackgroundColor,
+          )
+        ],
+      ),
     );
   }
 
